2edb936fdba722160680e6273eccd7baff170a2a
harrisb002 <harrisbe@sonoma.edu>
2024-10-31 13:06:51 -0700

    Adding buttons to slots

--- Scripts/Inventory/Inventory_slot.gd
+++ Scripts/Inventory/Inventory_slot.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_item_button_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_item_button_mouse_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_item_button_pressed() -> void:
+	pass # Replace with function body.
