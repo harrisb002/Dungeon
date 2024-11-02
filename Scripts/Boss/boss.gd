7cd02d3239e8bcabce247c811f137abe94020a36
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-11-01 18:32:20 -0700

    updated documentation

--- Scripts/Boss/boss.gd
+++ Scripts/Boss/boss.gd
@@ -5,12 +5,12 @@ extends CharacterBody2D
 @export var minion_count = 3
 var player_detected = false
 var player = null
-var timer: Timer
+var minion_timer: Timer
 var minions: Array = [] #keep track of spawned minions
 
 func _ready() -> void:
-	timer = $Timer  # Assuming the Timer node is a direct child
-	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+	minion_timer = $Minion_Timer
+	minion_timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 
 
 #need to add sprites. Probably attack death walk and idle
@@ -38,28 +38,30 @@ func _physics_process(delta: float) -> void:
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
 	player_detected = true
-	timer.start()
-	if not timer.is_stopped():
-		timer.stop()  # Stop the timer if it's already running
-	timer.start()  # Start or restart the timer
+	minion_timer.start()
+	if not minion_timer.is_stopped():
+		minion_timer.stop()  # Stop the timer if it's already running
+	minion_timer.start()  # Start or restart the timer
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_detected = false
-	timer.stop()
+	minion_timer.stop()
 	
-	
+
+#minion stuff
 func _on_timer_timeout() -> void:
 	#spawn minions here
 	spawn_minions()
 
 func spawn_minions() -> void:
-	if minion_scene and not are_minions_alive(minion_count):  # Check if the minion scene is assigned
+	if minion_scene and not are_minions_alive(minion_count):
 		var minion = minion_scene.instantiate()
-		minion.position = position  # Adjust spawn position as needed
-		get_parent().add_child(minion)  # Add minion to the same parent node
+		#minion spawn position
+		minion.position = position
+		get_parent().add_child(minion)
 		minions.append(minion)
-		timer.start()
+		minion_timer.start()
 		
 func are_minions_alive(max_count: int) -> bool:
 	var alive_count = 0
@@ -69,3 +71,13 @@ func are_minions_alive(max_count: int) -> bool:
 		if alive_count >= max_count:
 			return true
 	return false
+
+#need to add a way to remove minion from array on death
+
+#melee attack
+#make boss stop and play animation when in specific range of player
+#continue following player after untill in range again
+
+#ranged attack
+#make boss stop and play animation
+#shoot attack at player
