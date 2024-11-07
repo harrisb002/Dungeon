31e6e54711cb8370b39aa386e057161671ec3f5d
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-11-07 00:40:07 -0800

    Started on character selection
    Initial character selection scene added
    Changed starting scene to this one
    Rewrote some animation logic in Player to better fit multiple characters

---
+++ Scenes/Character_Select/character_selection.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_knight_pressed() -> void:
+	Global.character = "knight"
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+
+
+func _on_mage_pressed() -> void:
+	Global.character = "mage"
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
