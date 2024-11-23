c85d06f6159dc121388235ea798cf37ecdfd85f7
Meow <147790219+ReturnToSea@users.noreply.github.com>
2024-11-22 16:08:52 -0800

    added boss 2 hp

--- Scripts/Boss/boss_2.gd
+++ Scripts/Boss/boss_2.gd
@@ -16,6 +16,7 @@ const JUMP_VELOCITY = -400.0
 @export var next_state_index: int = 0
 var max_hp = 100
 var min_hp = 0
+var health = max_hp
 
 func _ready() -> void:
 	# Initialize the state machine, passing a reference of the player to the states,
