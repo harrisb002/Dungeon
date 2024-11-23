0c6f9aa0a5823cba19027346e901f9b32d0f661d
harrisb002 <harrisbe@sonoma.edu>
2024-11-22 18:21:54 -0800

    Adding item rewards to quests

--- Resources/Quest/Rewards.gd
+++ Resources/Quest/Rewards.gd
@@ -6,3 +6,12 @@ class_name Rewards
 
 @export var reward_type: String
 @export var reward_amount: int = 1  
+
+## Item based Rewards
+@export var reward_item_id: String
+@export var reward_item_quantity: int = 1
+
+## Example
+# reward_type="item", 
+# reward_item_id="common_key", 
+# reward_item_quantity=1
